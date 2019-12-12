fe7109ac839320770ab66eec361a4200c7da3eb4
selam ghide <esietselam@gmail.com>
2019-12-12 03:33:21 -0600

    java script

--- sample.js
+++ sample.js
@@ -1,8 +1,12 @@
-function init() { //function declaration
-    var name = "Mozilla";
-    function displayName() {
-    alert(name);
-    }
-    displayName();
-    }
+
   
+     var i;
+     var prod=1;
+    function product(){
+    for(i=1; i<arguments.length;i++){
+       prod=prod*argument[i];
+}
+    return prod;
+      }
+      
+      var x= product(2,3);
